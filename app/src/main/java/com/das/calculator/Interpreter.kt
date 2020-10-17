f6e3aa0522f291b5918ffbcb9fb65674df183db6
Daniel Schechtman <daschechtman@gmail.com>
2020-10-17 17:01:30 -0500

    switch the interpreter from using a number type represented by a single class to a group of classes that represent specific types (ie Int, Float, Double, etc)

--- app/src/main/java/com/das/calculator/Interpreter.kt
+++ app/src/main/java/com/das/calculator/Interpreter.kt
@@ -1,38 +1,42 @@
 package com.das.calculator
 
 import com.das.calculator.types.NumberType
+import com.das.calculator.types.numbers.DoubleType
+import com.das.calculator.types.numbers.FloatType
+import com.das.calculator.types.numbers.IntType
 
 class Interpreter {
     fun visit(node: Node): NumberType {
         return when (node) {
             is BinOpNode -> binOp(node)
             is NumberNode -> numberOp(node)
-            else -> null
+            else -> IntType(0)
         }
     }
 
     fun binOp(node: BinOpNode): NumberType {
-        var result: NumberDataType? = null
+        var result: NumberType = IntType(0)
         val left = visit(node.leftNode)
         val right = visit(node.rightNode)
 
         when (node.tok.tokType) {
             TT_PLUS -> result = left.add(right)
-            TT_MINUS -> result = left - right
-            TT_MUL -> result = left * right
-            TT_DIV -> result = left / right
+            TT_MINUS -> result = left.subtract(right)
+            TT_MUL -> result = left.multiply(right)
+            TT_DIV -> result = left.divide(right)
         }
 
 
         return result
     }
 
-    fun numberOp(node: NumberNode): NumberDataType? {
-        var number: NumberDataType? = null
-        if (node.tok.tokData != null) {
-            number = NumberDataType(node.tok.tokData)
-        }
-        return number
+    fun numberOp(node: NumberNode): NumberType {
+       return when(node.tok.tokType) {
+           TT_INT -> IntType(node.tok.tokData!! as Int)
+           TT_FLOAT -> FloatType(node.tok.tokData!! as Float)
+           TT_DOUBLE -> DoubleType(node.tok.tokData!! as Double)
+           else -> IntType(0)
+       }
     }
 
 }
